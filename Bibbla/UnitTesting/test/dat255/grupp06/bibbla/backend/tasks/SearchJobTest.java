2caa57e757e0e9104ed851065fb8d333d3a5d00b
Arild Matsson <arild@ekologik.se>
2012-10-04 15:37:21 +0200

    Testing to SearchJob, different input.

--- Bibbla/UnitTesting/test/dat255/grupp06/bibbla/backend/tasks/SearchJobTest.java
+++ Bibbla/UnitTesting/test/dat255/grupp06/bibbla/backend/tasks/SearchJobTest.java
@@ -1,5 +1,7 @@
 package dat255.grupp06.bibbla.backend.tasks;
 
+import java.util.List;
+
 import junit.framework.TestCase;
 import dat255.grupp06.bibbla.SampleSession;
 import dat255.grupp06.bibbla.backend.Session;
@@ -15,9 +17,26 @@ public class SearchJobTest extends TestCase {
 	
 	public void testRun() {
 		
-		SearchJob searchJob = new SearchJob("hej", session);
-		Message result = searchJob.run();
-		//assertWhat?
+		Message result;
 		
+		// Test normal string
+		SearchJob searchJob = new SearchJob("hej", session);
+		result = searchJob.run();
+		assertNotNull(result.obj);
+		assertTrue(result.obj instanceof List<?>);
+		assertFalse(((List<?>) result.obj).isEmpty());
+		
+		// Test weird string
+		SearchJob searchJobDifficult = new SearchJob("?^~QXZX", session);
+		result = searchJobDifficult.run();
+		assertNotNull(result.obj);
+		assertTrue(result.obj instanceof List<?>);
+		assertTrue(((List<?>)result.obj).isEmpty());
+		
+		// Test empty string
+		SearchJob searchJobEmpty = new SearchJob("", session);
+		result = searchJobEmpty.run();
+		assertNotNull(result.error);
+		// Perhaps we should check for more info?
 	}
 }
