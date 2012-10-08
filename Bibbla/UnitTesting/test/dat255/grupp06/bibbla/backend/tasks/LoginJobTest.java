87eb11219680736f18f419161f8953a1a4cb07d3
Niklas Logren <niklas@niklaslogren.com>
2012-10-08 11:01:49 +0200

    Test now passes.

--- Bibbla/UnitTesting/test/dat255/grupp06/bibbla/backend/tasks/LoginJobTest.java
+++ Bibbla/UnitTesting/test/dat255/grupp06/bibbla/backend/tasks/LoginJobTest.java
@@ -1,6 +1,7 @@
 package dat255.grupp06.bibbla.backend.tasks;
 
 import junit.framework.TestCase;
+import dat255.grupp06.bibbla.utils.Message;
 import dat255.grupp06.bibbla.utils.PrivateCredentials;
 
 public class LoginJobTest extends TestCase {
@@ -8,8 +9,8 @@ public class LoginJobTest extends TestCase {
 	public void testRun() {
 		// PrivateCredentials is an unpublished class in the tasks-package, for debugging purposes.
 		LoginJob job = new LoginJob(PrivateCredentials.name, PrivateCredentials.code, PrivateCredentials.pin);
-		Object result = job.run();
-		assertTrue((Boolean) result); // What would happen if job returned a different type? 
+		Message result = job.run();
+		assertTrue(result.loggedIn); // What would happen if job returned a different type? 
 	}
 
 }
