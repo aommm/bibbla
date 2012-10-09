a5769db09e69e9bf7ff24ef893fdab1907a207b6
Arild Matsson <arild@ekologik.se>
2012-10-09 11:21:07 +0200

    Drafted layout for logged-in Profile tab.

---
+++ Bibbla/src/dat255/grupp06/bibbla/fragments/ProfileFragment.java
@@ -0,0 +1,23 @@
+package dat255.grupp06.bibbla.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.actionbarsherlock.app.SherlockFragment;
+
+import dat255.grupp06.bibbla.R;
+
+/**
+ * Main fragment for the profile or "me" tab.
+ * @author arla
+ */
+public class ProfileFragment extends SherlockFragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.search_fragment, container, false);
+	}
+}
