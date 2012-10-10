619db645e6fb76482fe7f8c4a17ea710b501d958
Niklas Logren <niklas@niklaslogren.com>
2012-10-10 12:40:40 +0200

    added several methods, as well as implementations for fetchLoans(), fetchDetailedView() and getuserName().

--- Bibbla/src/dat255/grupp06/bibbla/backend/Backend.java
+++ Bibbla/src/dat255/grupp06/bibbla/backend/Backend.java
@@ -1,8 +1,11 @@
 package dat255.grupp06.bibbla.backend;
 
 import android.util.Log;
+import dat255.grupp06.bibbla.backend.tasks.DetailedViewJob;
+import dat255.grupp06.bibbla.backend.tasks.MyBooksJob;
 import dat255.grupp06.bibbla.backend.tasks.SearchJob;
 import dat255.grupp06.bibbla.backend.tasks.Task;
+import dat255.grupp06.bibbla.utils.Book;
 import dat255.grupp06.bibbla.utils.Callback;
 import dat255.grupp06.bibbla.utils.PrivateCredentials;
 
@@ -20,9 +23,25 @@ public class Backend {
 	private Session session;
 	
 	public Backend() {
-		 //network = new NetworkHandler(); // Don't need networkHandler? 
+		//network = new NetworkHandler(); // Don't need networkHandler? 
 		settings = new Settings(PrivateCredentials.name,PrivateCredentials.code,PrivateCredentials.pin);
-		 session = new Session(settings.getName(), settings.getCode(), settings.getPin());
+		session = new Session(settings.getName(), settings.getCode(), settings.getPin());
+	}
+	
+	/**
+	 *  @returns the user's username.
+	 *  If no name is saved, returns empty string.
+	 */
+	public String getUserName() {
+		return session.getName();
+	}
+	
+	/**
+	 *  @returns the user's current debt.
+	 *  TODO: Implement.
+	 */
+	public int getUserDebt() {
+		return (int)(Math.random()*500);
 	}
 	
 	/**
@@ -54,6 +73,53 @@ public class Backend {
 			// The code that's run in the Task (on new thread).
 			protected Void doInBackground(String... params) {
 				SearchJob job = new SearchJob(s);
+				message = job.run();
+				return null;
+			}
+		};
+		// Start the task.
+		task.execute();
+	}
+	
+	/**
+	 *  Fetches the DetailedView of the supplied book.
+	 *  Sends a new book with all the additional information to the callback.
+	 */
+	public void fetchDetailedView(final Book book, final Callback frontendCallback) {
+		// Create a new Task and define its body.
+		Task task = new Task(frontendCallback) {
+			@Override
+			// The code that's run in the Task (on new thread).
+			protected Void doInBackground(String... params) {
+				DetailedViewJob job = new DetailedViewJob(book);
+				message = job.run();
+				return null;
+			}
+		};
+		// Start the task.
+		task.execute();
+	}
+	
+	/**
+	 *  Fetches a list of the user's current reservations.
+	 *  Returns it using callback.
+	 *  TODO: implement.
+	 */
+	public void fetchReservations(Callback frontendCallback) {
+		throw new UnsupportedOperationException("implement pls");
+	}
+
+	/**
+	 *  Fetches a list of the user's currently loaned books.
+	 *  Returns it using callback.
+	 */
+	public void fetchLoans(Callback frontendCallback) {
+		// Create a new Task and define its body.
+		Task task = new Task(frontendCallback) {
+			@Override
+			// The code that's run in the Task (on new thread).
+			protected Void doInBackground(String... params) {
+				MyBooksJob job = new MyBooksJob(session);
 				message = job.run();
 				return null;
 			}
