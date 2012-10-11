02916d08baeaeafb0d258c6075f0fb97b9ebd899
Jonathan Orro <jonathan.orro@gmail.com>
2012-10-11 14:33:06 +0200

    Cleaned up some files

--- Bibbla/src/dat255/grupp06/bibbla/frontend/SearchFragment.java
+++ Bibbla/src/dat255/grupp06/bibbla/frontend/SearchFragment.java
@@ -5,19 +5,23 @@ import java.util.ArrayList;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TextView.OnEditorActionListener;
 
 import com.actionbarsherlock.app.SherlockFragment;
 
-import dat255.grupp06.bibbla.MainActivity;
 import dat255.grupp06.bibbla.R;
+import dat255.grupp06.bibbla.backend.Backend;
 import dat255.grupp06.bibbla.utils.Book;
 import dat255.grupp06.bibbla.utils.Callback;
 import dat255.grupp06.bibbla.utils.Message;
-import dat255.grupp06.bibbla.backend.Backend;
 
 /**
  * A fragment that holds the search bar, button 
@@ -30,12 +34,37 @@ public class SearchFragment extends SherlockFragment {
 
 	Backend backend;
 	SearchListFragment fragment;
+	EditText searchField;
+	Button searchButton;
 	
 	@Override
 	/**
 	 * Inflates the view and sets up the ListFragment
 	 */
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		
+		searchButton = (Button)getActivity().findViewById(R.id.search_button);
+		searchField = (EditText)getActivity().findViewById(R.id.search_field);
+
+		// Get text of "search" action from xml.
+		// TODO: change text of soft keyboard's next button.
+		String searchText = getActivity().getResources().getString(R.string.button_search); // Get text from xml
+
+		// Create a listener for keyboard "enter" button.
+		OnEditorActionListener listener = new OnEditorActionListener() {
+		    public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+		        if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+		            searchButton.performClick();
+		            return true;
+		        }
+		        return false;
+		    }
+		};
+		// Add our newly created listener to the text field.  
+		System.out.println("exists: "+(searchField != null));
+		System.out.println("instof: "+(searchField instanceof EditText));
+		// searchField.setOnEditorActionListener(listener);
+		// TODO: searchField doesn't exist!
 		
 		FragmentManager fragmentManager = getFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
@@ -50,7 +79,7 @@ public class SearchFragment extends SherlockFragment {
 	/** Starts searching procedure in backend. **/
 	public void search(String s) {
 		// Calls backend search, using callbacks.
-		backend.search(s, new Callback() {
+		backend.search(s, 0, new Callback() {
 			public void handleMessage(Message msg) {
 				SearchFragment.this.searchDone(msg);
 			}
