72dd7ce73a2cc87ffdbfcb3fa9af4fb2e3e6ba4f
Jonathan <jonathan.orro@gmail.com>
2012-10-11 20:58:38 +0200

    Started work on book overlay

---
+++ Bibbla/src/dat255/grupp06/bibbla/frontend/BookOverlayActivity.java
@@ -0,0 +1,19 @@
+package dat255.grupp06.bibbla.frontend;
+
+import android.os.Bundle;
+
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.Window;
+
+import dat255.grupp06.bibbla.R;
+
+public class BookOverlayActivity extends SherlockActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+    	setTheme(com.actionbarsherlock.R.style.Theme_Sherlock); //Used for theme switching in samples
+        requestWindowFeature(Window.FEATURE_ACTION_BAR_OVERLAY);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.overlay_book);
+    }
+}
