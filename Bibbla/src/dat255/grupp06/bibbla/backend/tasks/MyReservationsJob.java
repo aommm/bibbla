e5ba9766b9d8e1d25fe2fbc14a388d4bebb304eb
Niklas Logren <niklas@niklaslogren.com>
2012-10-11 16:19:16 +0200

    Removed unnecessary files.

---
+++ Bibbla/src/dat255/grupp06/bibbla/backend/tasks/MyReservationsJob.java
@@ -0,0 +1,5 @@
+package dat255.grupp06.bibbla.backend.tasks;
+
+public class MyReservationsJob {
+	// Implement me please
+}
---
+++ Bibbla/src/dat255/grupp06/bibbla/backend/tasks/UnreserveJob.java
@@ -0,0 +1,5 @@
+package dat255.grupp06.bibbla.backend.tasks;
+
+public class UnreserveJob {
+	// Implement me please 
+}
