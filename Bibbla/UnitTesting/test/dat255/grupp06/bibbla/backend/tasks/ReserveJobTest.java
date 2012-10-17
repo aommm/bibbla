1dae6e4610a3028ecb527887b9c13b3b1b285191
Arild Matsson <arild@ekologik.se>
2012-10-17 17:34:42 +0200

    Created empty ReserveJob test case.

---
+++ Bibbla/UnitTesting/test/dat255/grupp06/bibbla/backend/tasks/ReserveJobTest.java
@@ -0,0 +1,15 @@
+package dat255.grupp06.bibbla.backend.tasks;
+
+import junit.framework.TestCase;
+
+public class ReserveJobTest extends TestCase {
+
+	public void testReserveJob() {
+		fail("Not yet implemented");
+	}
+
+	public void testRun() {
+		fail("Not yet implemented");
+	}
+
+}
