22df1c7e0b21ff3803f3ca493eec2ec93b06f1c2
Arild Matsson <arild@ekologik.se>
2012-10-19 17:05:30 +0200

    Indentation in LoginOverlayActivity.java

--- Bibbla/src/dat255/grupp06/bibbla/frontend/LoginOverlayActivity.java
+++ Bibbla/src/dat255/grupp06/bibbla/frontend/LoginOverlayActivity.java
@@ -22,33 +22,33 @@ import dat255.grupp06.bibbla.model.Credentials;
 // TODO Make sure back button does something swell.
 public class LoginOverlayActivity extends SherlockActivity {
 
-		@Override
-		public void onCreate(Bundle savedInstanceState) {
-	    	setTheme(com.actionbarsherlock.R.style.Theme_Sherlock); //Used for theme switching in samples
-	        requestWindowFeature(Window.FEATURE_ACTION_BAR_OVERLAY);
-	        super.onCreate(savedInstanceState);
-	        setContentView(R.layout.activity_login_overlay);
-	        getSupportActionBar().setDisplayShowTitleEnabled(false);
-	        getSupportActionBar().setDisplayShowHomeEnabled(false);
-		}
-		
-		/**
-		 * This sets a result on an intent containing the user's input as a
-		 * Credentials object. The Credentials object is put as an extra on the
-		 * string specified by LoginManager.EXTRA_CREDENTIALS. 
-		 * @param view Unused.
-		 */
-		public void submit(View view) {
-			// Take care of input
-			EditText nameET = (EditText) findViewById(R.id.login_name_field);
-			EditText cardET = (EditText) findViewById(R.id.login_card_field);
-			EditText pinET = (EditText) findViewById(R.id.login_pin_field);
-			Credentials cred = new Credentials(nameET.getText().toString(),
-					cardET.getText().toString(), pinET.getText().toString());
-			// Send it back to whoever asked
-			Intent resultIntent = new Intent();
-			resultIntent.putExtra(MainActivity.EXTRA_CREDENTIALS, cred);
-			setResult(Activity.RESULT_OK, resultIntent);
-			finish();
-		}
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+    	setTheme(com.actionbarsherlock.R.style.Theme_Sherlock); //Used for theme switching in samples
+        requestWindowFeature(Window.FEATURE_ACTION_BAR_OVERLAY);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_overlay);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setDisplayShowHomeEnabled(false);
+	}
+	
+	/**
+	 * This sets a result on an intent containing the user's input as a
+	 * Credentials object. The Credentials object is put as an extra on the
+	 * string specified by LoginManager.EXTRA_CREDENTIALS. 
+	 * @param view Unused.
+	 */
+	public void submit(View view) {
+		// Take care of input
+		EditText nameET = (EditText) findViewById(R.id.login_name_field);
+		EditText cardET = (EditText) findViewById(R.id.login_card_field);
+		EditText pinET = (EditText) findViewById(R.id.login_pin_field);
+		Credentials cred = new Credentials(nameET.getText().toString(),
+				cardET.getText().toString(), pinET.getText().toString());
+		// Send it back to whoever asked
+		Intent resultIntent = new Intent();
+		resultIntent.putExtra(MainActivity.EXTRA_CREDENTIALS, cred);
+		setResult(Activity.RESULT_OK, resultIntent);
+		finish();
+	}
 }
