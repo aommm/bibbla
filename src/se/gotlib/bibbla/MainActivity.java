7c4be4695c797fb1d5e40f9ff7432135ae087c28
Jonis <jonathan.orro@gmail.com>
2014-10-19 12:58:12 +0200

    Created the layout for the main activity. Doesn't look so good but thats alright.

--- src/se/gotlib/bibbla/MainActivity.java
+++ src/se/gotlib/bibbla/MainActivity.java
@@ -1,14 +1,11 @@
 package se.gotlib.bibbla;
 
-import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.graphics.Point;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Display;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.LinearLayout.LayoutParams;
 
 import com.example.bibblatemp.R;
 
@@ -16,12 +13,40 @@ public class MainActivity extends Activity {
 
 	private Button searchButton, reservationsButton, loansButton, userButton, librariesButton, settingsButton;
 	
+	private OnClickListener buttonListener = new OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			switch(v.getId()) {
+			case R.id.search_button:
+				Intent i = new Intent(this, SearchActivity.class);
+				break;
+				
+			case R.id.reservations_button:
+				Intent i = new Intent(this, ReservationsActivity.class);
+				break;
+			
+			case R.id.loans_button:
+				Intent i = new Intent(this, LoansActivity.class);
+				break;
+				
+			case R.id.user_button:
+				Intent i = new Intent(this, UserActivity.class);
+				break;
+				
+			case R.id.libraries_button:
+				break;
+				
+			case R.id.settings_button:
+				break;
+			}
+		}
+	};
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-    	
     	initButtons();
     }
     
@@ -32,5 +57,11 @@ public class MainActivity extends Activity {
     	userButton = (Button)findViewById(R.id.user_button);
     	librariesButton = (Button)findViewById(R.id.libraries_button);
     	settingsButton = (Button)findViewById(R.id.settings_button);
+    	searchButton.setOnClickListener(buttonListener);
+    	reservationsButton.setOnClickListener(buttonListener);
+    	loansButton.setOnClickListener(buttonListener);
+    	userButton.setOnClickListener(buttonListener);
+    	librariesButton.setOnClickListener(buttonListener);
+    	settingsButton.setOnClickListener(buttonListener);
     }
 }
