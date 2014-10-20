6a33f5ab1fab1651e87ce297870c512c6959644a
Jonis <jonathan.orro@gmail.com>
2014-10-20 20:05:55 +0200

    Fixed appcompat

--- app/src/main/java/se/gotlib/bibbla/activities/ReservationsActivity.java
+++ app/src/main/java/se/gotlib/bibbla/activities/ReservationsActivity.java
@@ -3,16 +3,37 @@ package se.gotlib.bibbla.activities;
 import se.gotlib.bibbla.R;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class ReservationsActivity extends ActionBarActivity {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_reservations);
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reservations);
+
+        /*
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        mAdapter = new MyAdapter(myDataset);
+        mRecyclerView.setAdapter(mAdapter);
+        */
+    }
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
